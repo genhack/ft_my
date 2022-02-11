007f2cce3127e7ab3a0b0cfb4e1184442ee4a2ea
genhack <agliarulo@hotmail.it>
2022-02-11 20:50:26 +0100

    Update mod.py

--- mod.py
+++ mod.py
@@ -146,7 +146,7 @@ class Miku_PP_v3(IStrategy):
                         displacement=444, base_line_periods=88, laggin_span=88)
 
         create_ichimoku(dataframe, conversion_line_period=100,
-                        displacement=444, base_line_periods=100, laggin_span=100)
+                        displacement=544, base_line_periods=100, laggin_span=100)
 
         create_ichimoku(dataframe, conversion_line_period=355,
                         displacement=770, base_line_periods=355, laggin_span=355)
