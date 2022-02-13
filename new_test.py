c33bd51c338a46fe0a2bdf3644704022b5052bdc
genhack <agliarulo@hotmail.it>
2022-02-13 11:46:35 +0100

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -173,8 +173,8 @@ class HdGen(IStrategy):
            #(dataframe['kijun_sen_20'] > dataframe['tenkan_sen_444']) &
             (dataframe['senkou_a_9'] > dataframe['senkou_a_20']) &
             (dataframe['tenkan_sen_20'] >= dataframe['kijun_sen_20']) &
-            (dataframe['tenkan_sen_9'] >= dataframe['tenkan_sen_20']) &
-            (dataframe['tenkan_sen_9'] >= dataframe['kijun_sen_9'])
+           #(dataframe['tenkan_sen_9'] >= dataframe['tenkan_sen_20']) &
+           #(dataframe['tenkan_sen_9'] >= dataframe['kijun_sen_9'])
         ).astype('int')
       
         dataframe['trending_over'] = (
