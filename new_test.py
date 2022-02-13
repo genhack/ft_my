dc09e2dd767f2ce3b83e4f699dfa0f23ed1ef417
genhack <agliarulo@hotmail.it>
2022-02-13 16:39:11 +0100

    Update new_test.py

--- new_test.py
+++ new_test.py
@@ -172,7 +172,7 @@ class HdGen(IStrategy):
             (dataframe['senkou_b_100'] > dataframe['senkou_a_100']) &
             (dataframe['kijun_sen_355'] > dataframe['senkou_a_100']) &
            #(dataframe['kijun_sen_20'] >= dataframe['kijun_sen_355']) &
-            (dataframe['kijun_sen_9'] >= dataframe['senkou_b_100']) 
+            (dataframe['tenkan_sen_9'] >= dataframe['senkou_b_100']) 
            #(dataframe['kijun_sen_9'] > dataframe['senkou_b_100']) 
            #(dataframe['kijun_sen_20'] > dataframe['senkou_b_20']) &
            #(dataframe['senkou_a_9'] > dataframe['senkou_a_20']) &
